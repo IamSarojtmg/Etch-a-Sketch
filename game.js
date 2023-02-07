1ca6ee18b77334101b8aaf5c5c413abffc84f319
saroj <sarojtmg992@gmail.com>
2023-02-07 11:43:37 +0000

    mainDiv contents gets replaced

--- game.js
+++ game.js
@@ -1,31 +1,57 @@
+const contDiv = document.createElement('div')
 const mainDiv = document.createElement('div')
-mainDiv.classList.add('container')
-document.body.appendChild(mainDiv)
+// const gridDiv = document.createElement('div')
 
-function div ()
+document.body.appendChild(contDiv)
+contDiv.style.border = 'solid green';
+contDiv.style.display = 'inline-block'
+contDiv.appendChild(mainDiv)
+
+mainDiv.style.border = 'solid red';
+mainDiv.style.maxWidth = '500px'
+mainDiv.style.display = 'flex';
+mainDiv.style.flexWrap = 'wrap';
+
+
+
+for (let x = 1; x <=256; x++)
+{   
+    const gridDiv = document.createElement('div')
+    mainDiv.appendChild(gridDiv)
+    gridDiv.style.border = '1px solid black';
+    gridDiv.style.height = '30px'
+    gridDiv.style.width = '30px'
+}
+
+
+const btn = document.createElement('button')
+document.body.appendChild(btn)
+btn.style.width = '50px'
+btn.style.height = '20px'
+
+btn.addEventListener('click', ()=> 
 {
-    for(x = 1; x <=256; x++)
-    {
-        const smallBox = document.createElement('div')
-        mainDiv.appendChild(smallBox)
-        smallBox.classList.add('grid')
-        smallBox.addEventListener('mouseover', ()=>{
-            smallBox.style.backgroundColor = 'green'
-        })
+
+    let userInput = prompt('Enter between 1 and 100');
+    let multiply = userInput*userInput;
+
+    mainDiv.textContent = ''; // clears the contents of the mainDiv.
+
+    for (let x = 1; x <=multiply; x++)
+    {   
+        const newGridDiv = document.createElement('div')
+        mainDiv.appendChild(newGridDiv)
+        newGridDiv.style.border = '1px solid black';
+        newGridDiv.style.height = '30px'
+        newGridDiv.style.width = '30px'
+
+        // newGridDiv.addEventListener('mouseover', ()=>
+        // {
+        //     newGridDiv.style.backgroundColor = 'green'
+        // })
     }
     
-    
-    
-}
-div()
 
 
-
-
-const button = document.createElement('button')
-button.classList.add('button')
-document.body.appendChild(button)
-button.addEventListener('click', ()=>{
-    let userInput = prompt('Enter number between 0 - 100')
-    console.log(userInput);
 })
+
--- notes.md
+++ notes.md
@@ -1,5 +1,19 @@
-## Documentation
+# Documentation
 
  *Remember that doing a flex 1 will fill out your remaining space of you container. You were having this issue when trying to create 16x16 grid. Right now in the 16x16 you do not need flex because height and minWidth is taking care of the sizes and the wrap is filling the container evenly.*
 
  *Remember that the code that is local cannot be used outside its scope or globally. For e.g smallBox is inside for loop and I had to write the event listener inside it, for it to work.* 
+
+ ### **HOW TO MANUPLATE OUTER DIV TO RESIZE ACCORDING TO INNERDIV**
+- Turn the outerdiv display into inline-block
+    `outerDiv.style.display = 'inline-block'`
+
+
+### **MAX-WIDTH**
+- If the browser has a width higher than the declared max-width, then do not render the widht of the element any further. However if the browser width is smaller than the element itself then yes render the element thinner so that it can fit in the new width of the browser. 
+
+### **REPLACING DIV FIXED**
+- For this one this is what I did.
+    1. When the user clicks the button and inputs his data, the mainDiv contents gets cleared away.
+    2. Like this `mainDiv.textContent = '';`
+    3. Than the data entered by the user gets sent to the loop where it creates a newGridDiv and displays its contents inside the mainDiv.
