1ea0b1056fdb36d5ca3c0fd078a1bb74bf40447d
saroj <sarojtmg992@gmail.com>
2023-02-08 18:32:21 +0000

    mainDiv can be resized

--- game.js
+++ game.js
@@ -1,16 +1,50 @@
-const contDiv = document.createElement('div')
+const contDiv = document.createElement('section')
 const mainDiv = document.createElement('div')
 // const gridDiv = document.createElement('div')
 
 document.body.appendChild(contDiv)
-contDiv.style.border = 'solid green';
-contDiv.style.display = 'inline-block'
+// contDiv.style.border = 'solid goldenrod 15px';
+// contDiv.style.display = 'flex'
+// contDiv.style.justifyContent = 'center';
+contDiv.style.height = '600px';
+contDiv.style.width = '600px'
 contDiv.appendChild(mainDiv)
 
-mainDiv.style.border = 'solid red';
-mainDiv.style.maxWidth = '500px'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+mainDiv.style.height = '0';
+mainDiv.style.width = '100%';
+mainDiv.style.paddingBottom = '100%';
+mainDiv.style.border = 'red solid';
 mainDiv.style.display = 'flex';
 mainDiv.style.flexWrap = 'wrap';
+console.log(mainDiv);
 
 
 
@@ -21,6 +55,11 @@ for (let x = 1; x <=256; x++)
     gridDiv.style.border = '1px solid black';
     gridDiv.style.height = '30px'
     gridDiv.style.width = '30px'
+
+    gridDiv.addEventListener('mouseover', ()=>
+        {
+            gridDiv.style.backgroundColor = 'green'
+        })
 }
 
 
@@ -35,20 +74,25 @@ btn.addEventListener('click', ()=>
     let userInput = prompt('Enter between 1 and 100');
     let multiply = userInput*userInput;
 
+    console.log(mainDiv);
+
+
     mainDiv.textContent = ''; // clears the contents of the mainDiv.
 
     for (let x = 1; x <=multiply; x++)
     {   
         const newGridDiv = document.createElement('div')
         mainDiv.appendChild(newGridDiv)
+        // mainDiv.style.maxWidth = '500px'
+        console.log(mainDiv);
         newGridDiv.style.border = '1px solid black';
         newGridDiv.style.height = '30px'
         newGridDiv.style.width = '30px'
 
-        // newGridDiv.addEventListener('mouseover', ()=>
-        // {
-        //     newGridDiv.style.backgroundColor = 'green'
-        // })
+        newGridDiv.addEventListener('mouseover', ()=>
+        {
+            newGridDiv.style.backgroundColor = 'green'
+        })
     }
     
 
