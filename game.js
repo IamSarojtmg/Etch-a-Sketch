52b9995a4103c308c45061393ff2bdff7ece1f91
saroj <sarojtmg992@gmail.com>
2023-02-15 21:13:33 +0000

    completed

--- game.js
+++ game.js
@@ -1,98 +1,77 @@
 const contDiv = document.createElement('section')
 const mainDiv = document.createElement('div')
-// const gridDiv = document.createElement('div')
 
 document.body.appendChild(contDiv)
-// contDiv.style.border = 'solid goldenrod 15px';
-// contDiv.style.display = 'flex'
-// contDiv.style.justifyContent = 'center';
-contDiv.style.height = '600px';
-contDiv.style.width = '600px'
+contDiv.style.height = '500px';
+contDiv.style.width = '500px'
 contDiv.appendChild(mainDiv)
 
+mainDiv.style.border = 'gold solid 1px';
+mainDiv.style.display = 'grid';
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-mainDiv.style.height = '0';
-mainDiv.style.width = '100%';
-mainDiv.style.paddingBottom = '100%';
-mainDiv.style.border = 'red solid';
-mainDiv.style.display = 'flex';
-mainDiv.style.flexWrap = 'wrap';
-console.log(mainDiv);
-
-
+document.documentElement.style.setProperty('--columns-row', 16 )
 
 for (let x = 1; x <=256; x++)
+// 256 loop is hardcoded and only works on 16. In other words 16x16 = 256.
 {   
     const gridDiv = document.createElement('div')
     mainDiv.appendChild(gridDiv)
-    gridDiv.style.border = '1px solid black';
-    gridDiv.style.height = '30px'
-    gridDiv.style.width = '30px'
+    mainDiv.style.gridTemplateColumns = 'repeat(var(--columns-row),1fr)';
+    mainDiv.style.gridTemplateRows = 'repeat(var(--columns-row),1fr';
+
+
+    // gridDiv.style.border = '1px solid white';
+    gridDiv.style.aspectRatio = '1/1';
+    
 
     gridDiv.addEventListener('mouseover', ()=>
         {
-            gridDiv.style.backgroundColor = 'green'
+            gridDiv.style.backgroundColor = 'white'
         })
 }
-
 
 const btn = document.createElement('button')
 document.body.appendChild(btn)
 btn.style.width = '50px'
 btn.style.height = '20px'
+btn.textContent = 'Reset'
 
 btn.addEventListener('click', ()=> 
 {
-
     let userInput = prompt('Enter between 1 and 100');
     let multiply = userInput*userInput;
-
-    console.log(mainDiv);
-
-
     mainDiv.textContent = ''; // clears the contents of the mainDiv.
 
-    for (let x = 1; x <=multiply; x++)
-    {   
+    if(userInput >=100){
+        return mainDiv.style.border = '';
+    }
+
+    document.documentElement.style.setProperty("--columns-row", userInput);
+    
+    for (let x = 1; x <= multiply; x++)
+    {
         const newGridDiv = document.createElement('div')
         mainDiv.appendChild(newGridDiv)
-        // mainDiv.style.maxWidth = '500px'
-        console.log(mainDiv);
-        newGridDiv.style.border = '1px solid black';
-        newGridDiv.style.height = '30px'
-        newGridDiv.style.width = '30px'
 
-        newGridDiv.addEventListener('mouseover', ()=>
-        {
-            newGridDiv.style.backgroundColor = 'green'
-        })
+        mainDiv.style.gridTemplateColumns = 'repeat(var(--columns-row), 1fr)';
+        mainDiv.style.gridTemplateRows = 'repeat(var(--columns-row), 1fr)';
+   
+
+
+
+
+
+        // newGridDiv.style.border = '1px solid white';
+        newGridDiv.style.aspectRatio = '1/1'
+
+
+
+
+
+         newGridDiv.addEventListener('mouseover', ()=>
+         {
+             newGridDiv.style.backgroundColor = 'white'
+         })
     }
     
 
